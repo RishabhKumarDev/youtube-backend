1d96dc5c007b0bf401ed0d35aa3ace729370f832
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-09-02 22:09:03 +0530

    Wrapper Functions; Higher Order Function; Utilities

---
+++ utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+  return Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+};
+
+export default asyncHandler;
+
+// try/catch version
+
+asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (err) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+    next(err);
+  }
+};
