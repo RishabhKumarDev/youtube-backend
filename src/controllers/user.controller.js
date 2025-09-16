02ca7fcfd265e605fb43abc90abd92a14f6cf60b
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-09-16 22:04:31 +0530

    add: login controller, JWT vefify;

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -73,4 +73,85 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User Created Successfully!"));
 });
 
-export { registerUser };
+const generateAccessTokenAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Couldn't generate access token and refresh token");
+  }
+};
+const loginUser = asyncHandler(async (req, res) => {
+  // flow.
+  // 1. get user input(username, password);
+  // 2. validate inputs( valid input or not);
+  // 3. does user exist in the DB?
+  //  check if procided password is correct
+  // 3. generate refresh token/ access token;
+  // 4. put the token in the headers(auth: <bearer>); cookie(sccure);
+
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return new ApiError(400, "Username OR Email is required!!!");
+  }
+
+  let user = await User.findOne({ $or: [{ email }, { username }] });
+
+  if (!user) {
+    return new ApiError(404, "User Don't Exist in the DB");
+  }
+
+  let isPasswordVaild = await user.isPasswordCorrect(password);
+
+  if (!isPasswordVaild) {
+    return new ApiError(
+      401,
+      "Wrong Password, Please Enter a correct password!!!"
+    );
+  }
+
+  const { accessToken, refreshToken } =
+    await generateAccessTokenAndRefreshToken(user._id);
+
+  const logedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: logedInUser,
+          refreshToken,
+          accessToken,
+        },
+        "User Logged In Successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  //get the id
+  // get the user from DB
+  // empty the refresh token from his Doc
+  // expire access token
+});
+
+export { registerUser, loginUser };
