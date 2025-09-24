c8c8d55b7543f12584208c88b5f6d69bb0487487
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-09-24 21:23:18 +0530

    feat(user): add secure routes for changing password, getting current user, and updating account details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   refreshAccessToken,
   registerUser,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getChannelProfile,
+  getUserWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyAccessToken } from "../middlewares/auth.middlerware.js";
@@ -25,5 +32,23 @@ router.route("/login").post(loginUser);
 
 // secure routes
 router.route("/logout").post(verifyAccessToken, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyAccessToken, changeCurrentPassword);
+
+router.route("/current-user").get(verifyAccessToken, getCurrentUser);
+
+router.route("/update-account").patch(verifyAccessToken, updateAccountDetails);
+
+router
+  .route("/update-avatar")
+  .patch(verifyAccessToken, upload.single("avatar"), updateUserAvatar);
+  
+router
+  .route("/update-cover-image")
+  .patch(verifyAccessToken, upload.single("coverImage"), updateUserCoverImage);
+  
+router.route("/c/:username").get(verifyAccessToken, getChannelProfile);
+
+router.route("/watch-history").get(verifyAccessToken, getUserWatchHistory);
 export default router;
