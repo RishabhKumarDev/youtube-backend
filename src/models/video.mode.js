3d219f14be6ad9dc09a196277cfcc7d4c8a3d5ec
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-09-26 19:50:03 +0530

    feat(models): add Comment, Like, Playlist, and Tweet models

--- src/models/video.mode.js
+++ src/models/video.mode.js
@@ -44,4 +44,4 @@ const videoSchema = new mongoose.Schema(
 );
 
 videoSchema.plugin(mongooseAggregatePaginate);
-const Video = mongoose.model("Video", videoSchema);
+export const Video = mongoose.model("Video", videoSchema);
