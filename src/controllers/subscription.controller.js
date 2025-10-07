57cdcee79d987437a1467e1c5402e85e6e4cfc71
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-10-07 21:48:36 +0530

    chore(routes): scaffold boilerplate for routes and controller functions

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,7 @@
+import asyncHandler from "../utils/asyncHandler";
+
+const toggleSubscription = asyncHandler(async (req, res) => {});
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {});
+const getSubscribedChannels = asyncHandler(async (req, res) => {});
+
+export { toggleSubscription, getSubscribedChannels, getUserChannelSubscribers };
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { verifyAccessToken } from "../middlewares/auth.middlerware";
+import {
+  toggleSubscription,
+  getSubscribedChannels,
+  getUserChannelSubscribers,
+} from "../controllers/subscription.controller";
 
 const router = Router();
 
+router.use(verifyAccessToken);
 
+router.route("/c/:channelId").get(getSubscribedChannels).post(toggleSubscription)
 
+router.route("/u/:subscriberId").get(getUserChannelSubscribers);
 
 export default router;
